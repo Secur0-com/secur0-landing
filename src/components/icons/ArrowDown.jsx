f7faf90e6bd48bc624464aff403cd35bd6a82138
Dani X <danixidev@gmail.com>
2025-02-17 20:18:05 +0100

    Add robots.txt, smooth scrolling, and new icons; update layout and dependencies

---
+++ src/components/icons/ArrowDown.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ArrowDown as ArrowDownIcon } from 'lucide-react';
+
+const ArrowDown = () => {
+  return (
+      <ArrowDownIcon className="w-6 h-6" />
+  );
+};
+
+export default ArrowDown;
---
+++ src/components/icons/ArrowRight.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ArrowRight as ArrowRightIcon } from 'lucide-react';
+
+const ArrowRight = () => {
+  return (
+      <ArrowRightIcon className="w-6 h-6" />
+  );
+};
+
+export default ArrowRight;
